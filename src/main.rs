cc64694d84029042340413712e87d57e9bb8c808
eriksson <dev@der-klinger.de>
2021-02-05 23:03:34 +0100

    Finished day 3

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,7 @@ fn run(config: Config) ->  Result<(), Box<dyn Error>> {
     match config.day.as_str() {
         "day1" => day1(&contents),
         "day2" => day2(&contents),
+        "day3" => day3(&contents),
         _ => (),
     }
 
@@ -111,9 +112,24 @@ fn day2(contents: &str) {
             let count = count as u32;
             self.policy.0 <= count && count <= self.policy.1 
         }
+
+        fn validate_password1 (&self) -> bool {
+            let chars: Vec<(usize, char)> = self.password.char_indices().collect();
+            let pos_one = chars[self.policy.0  as usize -1];
+            let pos_two = chars[self.policy.1 as usize -1];
+
+            if pos_one.1 == self.letter && pos_two.1 != self.letter 
+                || pos_one.1 != self.letter && pos_two.1 == self.letter {
+                return true
+            } 
+           
+            false
+        }
     }
 
     let mut count = 0;
+
+    let mut count_1 = 0;
     for line in contents.lines() {
         let mut iter = line.split_ascii_whitespace();
 
@@ -150,12 +166,71 @@ fn day2(contents: &str) {
 
         let result = entry.validate_password();
 
+        let result1 = entry.validate_password1();
+
         //println!("{}", result);
 
         if result {
             count = count + 1;
         }
-    }
-    println!("{}", count)
 
+        if result1 {
+            count_1 = count_1 + 1;
+        }
+    }
+    println!("{}", count);
+    println!("{}", count_1)
+}
+
+fn day3(contents: &str) {
+    let lines = contents.lines();
+
+    let mut base: Vec<Vec<char>> = vec![];
+    for line in lines {
+        let second_dimension: Vec<char> = line.chars().collect();
+        base.push(second_dimension);
+    }
+
+    fn check_slope(terrain: &mut  Vec<Vec<char>>, row_increment: usize, column_increment: usize) -> usize {
+        let compare = '#';
+        let mut count = 0;
+        let mut row_index = 0;
+        let mut column_index = 0;
+        while row_index < terrain.len() {
+            if terrain[row_index][column_index % terrain[0].len()] == compare {
+            count = count + 1;
+            }
+
+        row_index = row_index + row_increment;
+        column_index = column_index + column_increment;
+        }
+
+        println!("Count of #: {}", count);
+
+
+        return count;
+    }   
+
+    let mut row_increment = 1;
+    let mut column_increment = 1;
+
+    let count_1 = check_slope(& mut base, row_increment, column_increment);
+
+    column_increment = 3;
+    let count_2 = check_slope(& mut base, row_increment, column_increment);
+
+    column_increment = 5;
+    let count_3 = check_slope(& mut base, row_increment, column_increment);
+
+    column_increment = 7;
+    let count_4 = check_slope(& mut base, row_increment, column_increment);
+
+    column_increment = 1;
+    row_increment = 2;
+    let count_5 = check_slope(& mut base, row_increment, column_increment);
+
+    let counts_multiplied  = count_1 * count_2 * count_3 * count_4 * count_5; 
+
+    println!("Count of # with 1,3: {}", count_2);
+    println!("Count of #: {}", counts_multiplied);
 }
